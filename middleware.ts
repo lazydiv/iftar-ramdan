f124bca1b6cd37e66598f30900b19407ddbbac93
lazydiv <yhiazedan555@gmail.com>
2024-03-22 16:24:57 +0200

    updating fonts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 
 import { authMiddleware } from "@clerk/nextjs";
 
+
+
 export default authMiddleware({
   // An array of public routes that don't require authentication.
   publicRoutes: ["/org", '/', ],
@@ -8,6 +10,8 @@ export default authMiddleware({
   // An array of routes to be ignored by the authentication middleware.
   ignoredRoutes: ["/api/webhook/clerk", ],
 });
+
+
 
 export const config = {
   
